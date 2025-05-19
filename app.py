f9885215631b86c8caaa6560683d894496383193
Nikhil172913832 <nikhilarora13832@gmail.com>
2025-05-19 23:44:09 +0530

    further

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/'):
+@app.route('/')
 def home():
     return "Hi, welcome to test app"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
