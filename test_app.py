394465defbce182eee186b9392a956cde5e42a0d
Nikhil172913832 <nikhilarora13832@gmail.com>
2025-05-19 23:45:34 +0530

    further

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello" in response.data
+    assert b"Hi" in response.data
 
